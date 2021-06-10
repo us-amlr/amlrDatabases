78668be001716accef28a812614eeac02fc8503e
Sam Woodman <sam.woodman@noaa.gov>
2021-06-10 14:38:06 -0700

    Initial package commit

---
+++ R/amlrDatabases-package.R
@@ -0,0 +1,21 @@
+#' amlrDatabases: General functions used by other AMLR packages
+#'
+#' General functions used by other AMLR packages
+#'
+#' @name amlrDatabases-package
+#' @aliases amlrDatabases
+#' @docType package
+#' @title General Functions for AMLR Database Packages
+#' @author Sam Woodman \email{sam.woodman@@noaa.gov}
+#'
+#' @import shiny
+#'
+#' @importFrom dplyr %>% select
+#' @importFrom DT DTOutput renderDT
+#' @importFrom ggplot2 ggsave
+#' @importFrom pool dbGetQuery
+#' @importFrom shinydashboard box
+#' @importFrom utils write.csv
+#'
+#' @keywords package
+NULL
---
+++ R/amlr_dbPool.R
@@ -0,0 +1,34 @@
+#' Wrapper to connect to an AMLR database
+#'
+#' Wrapper with standard, AMLR defaults for connecting to an AMLR database
+#'
+#' @param Database character; see \code{\link[pool]{dbPool}}
+#' @param Driver character; default is 'SQL Server'. See \code{\link[pool]{dbPool}}
+#' @param Server character; default is 'swc-estrella-s'. See \code{\link[pool]{dbPool}}
+#' @param idleTimeout integer; default is one hour. See \code{\link[pool]{dbPool}}
+#' @param ... additional arguments passed to \link[pool]{dbPool})
+#'
+#' @details Wrapper for a call to \link[pool]{dbPool})
+#'
+#' @return Output of \code{\link[base]{try}(\link[pool]{dbPool})} call
+#'
+#' @examples
+#' \dontrun{
+#' amlr_dbPool("AMLR_PINNIPEDS")
+#' }
+#'
+#' @seealso https://github.com/rstudio/pool
+#'
+#' @export
+amlr_pinnipeds_dbPool <- function(Database, Driver = "SQL Server", Server = "swc-estrella-s",
+                                  idleTimeout = 3600000, ...) {
+  try(pool::dbPool(
+    drv = odbc::odbc(),
+    Driver = Driver,
+    Server = Server,
+    Database = Database,
+    Trusted_Connection = "True",
+    idleTimeout = idleTimeout, #3600000  # 1 hour
+    ...
+  ), silent = TRUE)
+}
